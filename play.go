d99f651228a7f04fb143f2e6dbda1c424c790ca7
deepak987 <14358707+deepak987@users.noreply.github.com>
2020-04-09 01:42:12 -0400

    Gameplay random player (#2)
    Create a gameplay framework, use two players who select random squares to validate
    * get empty squares
    
    * gameplay working with two random players

--- play.go
+++ play.go
@@ -3,7 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	a := gameBoard{}
-	fmt.Println("Lets go!")
-	fmt.Println(a)
+	b := initGameBoard()
+	b.player1 = randomPlayer{playerNumber: 1}
+	b.player2 = randomPlayer{playerNumber: -1}
+	play(b)
+}
+
+func play(g gameBoard) {
+	gameOver := false
+	winner := 0
+	movesPlayed := 0
+	for !gameOver {
+		if movesPlayed%2 == 0 {
+			g.player1.makeMove(g)
+		} else {
+			g.player2.makeMove(g)
+		}
+		movesPlayed++
+		gameOver, winner = g.checkGameOver()
+		g.displayBoard()
+		fmt.Println()
+	}
+	if winner == 1 {
+		fmt.Println("Winner: X")
+	} else if winner == -1 {
+		fmt.Println("Winner: O")
+	} else {
+		fmt.Println("Draw")
+	}
+
 }
--- players.go
+++ players.go
@@ -1,7 +1,12 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 type ticTacToePlayer interface {
-	chooseMove() // Return a co-ordinate for
+	makeMove(g gameBoard)
 }
 type randomPlayer struct {
 	playerNumber int
@@ -11,10 +16,15 @@ type humanPlayer struct {
 	playerNumber int
 }
 
-func (r randomPlayer) chooseMove(b gameBoard) {
-
+func (r randomPlayer) makeMove(b gameBoard) {
+	moveOptions := b.getAllEmptyLocations()
+	s := rand.NewSource(time.Now().Unix())
+	rng := rand.New(s) // initialize local pseudorandom generator
+	moveIndex := rng.Intn(len(moveOptions))
+	x, y := moveOptions[moveIndex].x, moveOptions[moveIndex].y
+	b.board[x][y] = r.playerNumber
 }
 
-func (h humanPlayer) chooseMove(b gameBoard) {
+func (h humanPlayer) makeMove(b gameBoard) {
 
 }
